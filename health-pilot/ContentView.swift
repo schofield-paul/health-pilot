0e4fcea6f9b75b02ab943691d8b0641b897ffa68
Paul Schofield <paul.schofield.us@gmail.com>
2024-10-26 20:05:56 -0400

    simple breathing UI

--- health-pilot/ContentView.swift
+++ health-pilot/ContentView.swift
@@ -36,10 +36,48 @@ struct FirstPageView: View {
                             .frame(width: CGFloat(100 + index * 20), height: CGFloat(100 + index * 20))
                     }
                 }
-
+                
                 Spacer()
+                
+                NavigationLink(destination: SecondPageView()) {
+                    Text("Start")
+                        .font(.title2)
+                        .foregroundColor(.blue)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 40)
+                }
+                .padding(.bottom, 50)
             }
             .padding()
+        }
+    }
+}
+
+
+struct SecondPageView: View {
+    @State private var isExpanded = false
+
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .frame(width: isExpanded ? 200 : 10, height: isExpanded ? 200 : 10)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation(.easeInOut(duration: 3)) {
+                            isExpanded = true
+                        }
+                        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { _ in
+                            withAnimation(.easeInOut(duration: 3)) {
+                                isExpanded.toggle()
+                            }
+                        }
+                    }
+                }
         }
     }
 }
